4d1a3c1125aef882be2402f050a66c51edeac1e9
diegodonascimentoconfessor <153656081+diegodonascimentoconfessor@users.noreply.github.com>
2024-06-11 19:20:29 -0300

    contador implemetado

--- aumentar_texto.js
+++ aumentar_texto.js
@@ -1,19 +1,10 @@
-function aumentartexto() {
-    const elemento = document.getElementById("texto");
-    const tamanhoAtual = parseInt(window.getComputedStyle(elemento, null).getPropertyValue('font-size'));
-    const novoTamanho = tamanhoAtual + 5;
-    elemento.style.fontSize = novoTamanho + 'px';
+let fontSize = 16; 
+const maxFontSize = 30; 
+
+function aumentarTexto() {
+    if (fontSize < maxFontSize) {
+        fontSize += 2; // 
+        document.documentElement.style.fontSize = fontSize + "px";
+    }
 }
 
-
-const botao = document.getElementsByTagName('button');
-
-botao[0].addEventListener('click', () => document.body.style.fontSize = '10px');
-botao[1].addEventListener('click', () => document.body.style.fontSize = '15px');
-botao[2].addEventListener('click', () => document.body.style.fontSize = '20px');
-
-botao[3].addEventListener('click', () => {
-    aumentartexto();
-    aumentartexto();
-    aumentartexto();
-});
